a0a376060c2a418df0318a17f148d5c5ec03a6b8
Your Name <saul.vera787@gmail.com>
2024-12-08 17:55:43 -0400

    Calculating the shape of a 3d matrix

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to calculate the shape of a NumPy matrix.
+"""
+
+
+import numpy as np
+
+
+# Using shape attribute to calculate shape of matrices
+def np_shape(matrix):
+    """
+    Calculates the shape of a NumPy matrix.
+
+    Args:
+        matrix (np.ndarray): The NumPy matrix.
+
+    Returns:
+        tuple: A tuple representing the shape of the matrix.
+    """
+    return matrix.shape
