41216182d7d4b960e1bc702f463239c56111caf1
Your Name <saul.vera787@gmail.com>
2024-12-09 12:14:55 -0400

    A function that transpose a matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to transpose a 2D matrix.
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a matrix.
+
+    Args:
+        matrix (list of lists): A 2D matrix to transpose.
+
+    Returns:
+        list of lists: The transposed matrix.
+    """
+    return np.transpose(matrix)
