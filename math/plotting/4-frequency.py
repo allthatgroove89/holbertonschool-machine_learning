9c1c97bb85dbf2bd0f2a4edeedc97eb691b64306
Your Name <saul.vera787@gmail.com>
2024-12-11 15:33:09 -0400

    Completing source code to plot a histogram of student scores for a project

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""This module provides a function to plot
+a histogram of student scores for a project.
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+
+
+def frequency():
+    """
+    Plots a histogram of student scores for a project.
+
+    The x-axis is labeled 'Grades'.
+    The y-axis is labeled 'Number of Students'.
+    The x-axis has bins every 10 units.
+    The title of the plot is 'Project A'.
+    The bars are outlined in black.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Create an histogram plot
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
+    # Create title and labels
+    plt.title('Project A')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+
+    # Setting axis ranges
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+
+    # Display histogram
+    plt.show()
